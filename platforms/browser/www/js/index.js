e2e80aad96d5b3520a5320bdfd6c1dae7dc79351
prabhu <prabhu@prabhus-MacBook-Pro.local>
2017-05-08 14:40:34 +0530

    first commit

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -69,6 +69,8 @@ var app = {
 
             listeningElement.setAttribute('style', 'display:none;');
             receivedElement.setAttribute('style', 'display:block;');
+                        receivedElement.setAttribute('html', data.registrationId);
+
         });
 
         push.on('error', function(e) {
--- www/js/index.js
+++ www/js/index.js
@@ -69,6 +69,7 @@ var app = {
 
             listeningElement.setAttribute('style', 'display:none;');
             receivedElement.setAttribute('style', 'display:block;');
+        
         });
 
         push.on('error', function(e) {
