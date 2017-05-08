1dc4b38aad4f9742c89ea05427c9b6729e53e128
prabhu <prabhu@prabhus-MacBook-Pro.local>
2017-05-08 15:53:24 +0530

    first commit

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -33,15 +33,25 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        console.log('Received Device Ready Event');
-        console.log('calling setup push');
+        app.receivedEvent('deviceready');
         app.setupPush();
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
     },
     setupPush: function() {
         console.log('calling push init');
         var push = PushNotification.init({
             "android": {
-                "senderID": "322154966713"
+                "senderID": "739332954225"
             },
             "browser": {},
             "ios": {
@@ -63,7 +73,7 @@ var app = {
                 alert(data.registrationId);
                 // Post registrationId to your app server as the value has changed
             }
-alert(data.registrationId);
+            alert(data.registrationId);
             var parentElement = document.getElementById('registration');
             var listeningElement = parentElement.querySelector('.waiting');
             var receivedElement = parentElement.querySelector('.received');
