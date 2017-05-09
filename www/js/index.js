1f2a602c524ae20c949c444d366c372184e038bd
prabhu <prabhu@prabhus-MacBook-Pro.local>
2017-05-09 10:13:21 +0530

    first commit

--- www/js/index.js
+++ www/js/index.js
@@ -41,7 +41,7 @@ var app = {
         console.log('calling push init');
         var push = PushNotification.init({
             "android": {
-                "senderID": "322154966713"
+                "senderID": "677437474748"
             },
             "browser": {},
             "ios": {
