04513c950e12c08e3da59c9093179f2a19026eae
prabhu <prabhu@prabhus-MacBook-Pro.local>
2017-05-15 18:18:03 +0530

    prabhu

--- www/js/index.js
+++ www/js/index.js
@@ -45,5 +45,29 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+alert( 'Received Event' );
+
+
+        FCMPlugin.onTokenRefresh(function(token){
+            alert( token );
+        });
+FCMPlugin.getToken(function(token){
+    alert(token);
+});
+
+FCMPlugin.onNotification(function(data){
+    if(data.wasTapped){
+      //Notification was received on device tray and tapped by the user.
+      alert( JSON.stringify(data) );
+    }else{
+      //Notification was received in foreground. Maybe the user needs to be notified.
+      alert( JSON.stringify(data) );
+    }
+});
+
+
+
+
+
     }
 };
